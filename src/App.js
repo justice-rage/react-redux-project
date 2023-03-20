29814baa9be8db48274ba560ad143fcd2dcddb9a
justicepelteir <justice.douglas@hey.com>
2023-03-20 14:11:58 -0700

    add two state slices to store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Fragment } from 'react';
+import Header from './components/Header';
 import Counter from './components/Counter';
-
+import Auth from './components/Auth';
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
   );
 }
 
